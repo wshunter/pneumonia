9a47caec96dc59803213ab254457066a7a76ac58
wshunter <whunter554@gmail.com>
2023-06-12 20:32:03 -0700

    init

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import torch
 from model import *
-
+import matplotlib.pyplot as plt
 device = (
     "cuda"
     if torch.cuda.is_available()
@@ -14,8 +14,18 @@ IM_SIZE=32
 def load(path):
     en = encoder(IM_SIZE*IM_SIZE,256,8)#.load_state_dict(torch.load(f"{path}_en.pt"))
     de = decoder(8,256,IM_SIZE*IM_SIZE)#.load_state_dict(torch.load(f"{path}_de.pt"))
-    mo = model(en,de,device)
+    mo = model(en,de,device).to(device)
     mo.load_state_dict(torch.load(f"{path}.pt"))
     return mo
 
 mod = load("/home/wcsng/hwk/chest_xray/easy")
+mod.eval()
+
+with torch.no_grad():
+    point = torch.randn(8).to(device)
+    im = mod.decoder.forward(point).cpu().numpy().reshape((32,32))
+
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.imshow(im)
+plt.show()
